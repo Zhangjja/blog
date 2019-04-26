1da67487edc71b826215f91428c9ebdc1886213c
junjie.zhang@ulsee.com <junjie.zhang@ulsee.com>
2019-04-26 16:05:15 +0800

    update

--- shell/v2ray/contorl_vultr.sh
+++ shell/v2ray/contorl_vultr.sh
@@ -9,15 +9,20 @@ cat logfile
 
 while true
 do
-	read -p "选择要删除的服务器SUBID : " subid
-	count=`cat logfile | grep -E "\b$subid\b" |wc -l`
-	if [ $count -gt 0 ]
+	read -p "选择要删除的服务器SUBID 取消输入[n] : " subid
+	if [ $subid == "n" ]
 	then
-		curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/destroy --data "SUBID=$subid"
-		echo "SUBID: $subid 删除成功"
 		break
 	else
-		echo "$subid 不存在"
+		count=`cat logfile | grep -E "\b$subid\b" |wc -l`
+		if [ $count -gt 0 ]
+		then
+			curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/destroy --data "SUBID=$subid"
+			echo "SUBID: $subid 删除成功"
+			break
+		else
+			echo "$subid 不存在"
+		fi
 	fi
 done
 
