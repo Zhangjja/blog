dc6ffecb15ad3cbb6f6167359a3f11d983806453
junjie.zhang@ulsee.com <junjie.zhang@ulsee.com>
2019-05-02 20:22:59 +0800

    update

--- shell/v2ray/deploy_vps.sh
+++ shell/v2ray/deploy_vps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+key="API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA"
 vps=""
 if [ -z "$1" ]
 then
@@ -7,24 +8,23 @@ else
 	vps="$1"
 fi
 
-	curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/create --data 'DCID=5' --data 'VPSPLANID=201' --data 'OSID=193' --data 'SSHKEYID=5cc15103d9f4d' --data "label=$vps" > subid.txt
-		cat subid.txt
-		echo "installing server......"
+curl -H "$key" https://api.vultr.com/v1/server/create --data 'DCID=5' --data 'VPSPLANID=201' --data 'OSID=193' --data 'SSHKEYID=5cc15103d9f4d' --data "label=$vps" > subid.txt
+cat subid.txt
+echo "installing server......"
 
 SUBID=`cat subid.txt | awk -F '"' '{print$4}'`
 echo $SUBID
 
-
 ip=""
 while true
 do
-	ip=`curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/list?SUBID=$SUBID | python -m json.tool | grep -E "\bmain_ip\b"  |awk -F '"' '{print$4}'`
+	ip=`curl -H "$key" https://api.vultr.com/v1/server/list?SUBID=$SUBID | python -m json.tool | grep -E "\bmain_ip\b"  |awk -F '"' '{print$4}'`
 	if [ $ip == "0.0.0.0" ]
 	then	
 		echo $ip
 		echo "getting ip......"
 		sleep 5
-		ip=`curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/list?SUBID=$SUBID | python -m json.tool | grep -E "\bmain_ip\b"  |awk -F '"' '{print$4}'`
+		ip=`curl -H "$key" https://api.vultr.com/v1/server/list?SUBID=$SUBID | python -m json.tool | grep -E "\bmain_ip\b"  |awk -F '"' '{print$4}'`
 	else
 		echo $ip
 		break
@@ -35,13 +35,13 @@ done
 pwd=""
 while true
 do
-	pwd=`curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/list?SUBID=$SUBID  | python -m json.tool | grep -E  "\bdefault_password\b" |awk -F '"' '{print$4}'`
+	pwd=`curl -H "$key" https://api.vultr.com/v1/server/list?SUBID=$SUBID  | python -m json.tool | grep -E  "\bdefault_password\b" |awk -F '"' '{print$4}'`
 	if [ -z $pwd ]
 	then
 		echo $pwd
 		echo "getting pwd......"
 		sleep 5
-		pwd=`curl -H 'API-Key: R336LW2OYPBDWOAYEPYUKD6EX4ZS37RHFQBA' https://api.vultr.com/v1/server/list?SUBID=$SUBID  | python -m json.tool | grep -E  "\bdefault_password\b" |awk -F '"' '{print$4}'`
+		pwd=`curl -H "$key" https://api.vultr.com/v1/server/list?SUBID=$SUBID  | python -m json.tool | grep -E  "\bdefault_password\b" |awk -F '"' '{print$4}'`
 	else
 		echo $pwd
 		break
@@ -83,22 +83,10 @@ then
 			ssh -o stricthostkeychecking=no root@$ip "ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa -q"
 		fi
 	done
-#	echo "将本地密钥拷贝到腾讯云上"
-#	ssh-copy-id -i /root/.ssh/id_rsa.pub ubuntu@118.89.139.53
-#	echo "远程v2ray服务器，并配置v2ray与腾讯云的免密ssh登录"
-#	ssh -t root@$ip  "ssh-copy-id -i /root/.ssh/id_rsa.pub ubuntu@118.89.139.53"
-#	echo "将本地密钥拷贝到v2ray服务端"
-#	ssh-copy-id -i /root/.ssh/id_rsa.pub root@$ip
-#	echo "添加密钥成功！"
+
 	echo "------------"
 	echo "正在配置ansible主控端"
-#	read -p "请输入被控端密码：" pwd
-#	count=`sed -n "/$ip/p" /etc/ansible/hosts | wc -l`
-#	if [ $count -ge 0 ]
-#	then
-#		echo "$ip exsiting in /etc/ansible/hosts"
-#		sed -i /$ip/d /etc/ansible/hosts
-#	fi
+
 	jugde=`cat /etc/ansible/hosts | grep -E "\b$vps\b"| wc -l`
 	if [ $jugde -gt 0 ]
 	then
@@ -122,40 +110,9 @@ then
 		echo "主控端配置失败！"
 		exit 1
 	fi
-#	echo "配置腾讯云与vps的免密认证"
-#	ubuntu_key=`ansible tx -m shell -a "cat /home/ubuntu/.ssh/id_rsa.pub " |grep ubuntu@VM-0-7-ubuntu`
-#	echo "$ubuntu_key"
-#	count=`ansible $vps -m shell -a "cat /root/.ssh/authorized_keys" |grep ubuntu@VM-0-7-ubuntu | wc -l`
-#	echo "$count"
-#	if [ $count -gt 0 ]
-#	then
-#		echo "vps已经存在腾讯云的密钥！"
-#		echo "删除vps已存在的腾讯云密钥！"
-#		ansible $vps -m shell -a "sed -i '/ubuntu@VM-0-7-ubuntu/d' /root/.ssh/authorized_keys"
-#	fi
-#	echo "配置vps中的腾讯云密钥"
-#	ansible $vps -m shell -a "sed -i '\$a\\$ubuntu_key' /root/.ssh/authorized_keys"
-#	echo "vps密钥配置成功！"
-#	echo "vps与腾讯云的免密认证"
-#	vps_key=`ansible $vps -m shell -a "cat /root/.ssh/id_rsa.pub " | grep root@`
-#	echo "$vps_key"
-#	tag=`ansible $vps -m shell -a 'cat /root/.ssh/id_rsa.pub' | sed -n '/ssh-rsa/p' |awk '{print $3}'`
-#	count=`ansible tx -m shell -a "cat /home/ubuntu/.ssh/authorized_keys" | grep "$tag" | wc -l`
-#	echo "$count"
-#	if [ $count -gt 0 ]
-#	then
-#		echo "腾讯云已经存在ps的密钥！"
-#		echo "删除腾讯云已存在的vps密钥！"
-#		ansible tx -m shell -a "sed -i "/$tag/d" /home/ubuntu/.ssh/authorized_keys"
-#	fi
-#	echo "配置腾讯云中的vps密钥"
-#	ansible tx -m shell -a "sed -i '\$a\\$vps_key' /home/ubuntu/.ssh/authorized_keys"
-#	echo "腾讯云密钥配置成功！"
 
-	echo "-------------------"
 	echo "开始安装v2ray服务端"
-	#配置v2ray服务端脚本,使用ansible script模块路径为./v2ray_server_install.sh
-	#安装v2ray服务端
+	#配置v2ray服务端脚本,使用ansible script模块路径为./v2ray_server_install.sh,开始安装v2ray服务端
 	ansible $vps -m script -a "./v2ray_server_install.sh"
 	ansible $vps -m copy -a "src=./server_config.json dest=/etc/v2ray/config.json owner=root group=root mode=0644"
 	echo "v2ray服务端安装完毕！"
@@ -167,21 +124,18 @@ then
 	id=`ansible $vps -m shell -a "cat /root/config.json.bak" |sed -n '9p'`
 	ansible $vps -m shell -a "cat  /root/config.json.bak"
 	ansible $vps -m shell -a "cat  /etc/v2ray/config.json"
-	echo "$ip"
-	echo "$port"
-	echo "$id"
-	echo "正在修改------"
+	echo "$ip"  "$port"   "$id"
+	echo "正在修改-----------"
 	ansible $vps -m shell -a "sed -i '8i\\$port' /etc/v2ray/config.json"
 	ansible $vps -m shell -a "sed -i '9d' /etc/v2ray/config.json"
 	ansible $vps -m shell -a "sed -i '13i\\$id' /etc/v2ray/config.json"
 	ansible $vps -m shell -a "sed -i '14d' /etc/v2ray/config.json"
 	echo "修改完毕-----------"
-	echo "v2ray服务端配置成功!"
-	echo "重启v2ray服务器"
+	echo "v2ray服务端配置成功!开始重启v2ray服务器"
 	ansible $vps -m shell -a "reboot"
 	echo "重启成功！"
 	echo "-------------------"
 else
-	echo "ip error, please contiue"
+	echo "ip error, please check"
 	exit 1
 fi
